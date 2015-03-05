71504e364ade83a867b9abe5be6f4297e03e61a6
Miguel Martinez-Alvarez <miguel.martinez@signal.uk.com>
2015-03-05 21:21:43 +0000

    Refactor config and arguments parsing

--- src/run.py
+++ src/run.py
@@ -5,15 +5,18 @@ import wget
 import argparse
 import configparser
 
-#TODO: General refactor
-parser = argparse.ArgumentParser(description='Download pictures from a Twitter feed.')
-parser.add_argument('username', type=str, help='the twitter screen name from the account we want to retrieve all the pictures')
-args = parser.parse_args()
+def parse_arguments():
+	parser = argparse.ArgumentParser(description='Download pictures from a Twitter feed.')
+	parser.add_argument('username', type=str, help='the twitter screen name from the account we want to retrieve all the pictures')
+	args = parser.parse_args()
+	return args
 
-config = configparser.ConfigParser()
-config.read('config.cfg')
-
-username = args.username
+def parse_config(config_file):
+	config = configparser.ConfigParser()
+	config.read(config_file)
+	# TODO: Return a dictionary with all the key-values
+	return config 
+	
 
 @classmethod
 def parse(cls, api, raw):
@@ -21,37 +24,45 @@ def parse(cls, api, raw):
     setattr(status, 'json', json.dumps(raw))
     return status
 
-# Status() is the data model for a tweet
-tweepy.models.Status.first_parse = tweepy.models.Status.parse
-tweepy.models.Status.parse = parse
-# User() is the data model for a user profil
-tweepy.models.User.first_parse = tweepy.models.User.parse
-tweepy.models.User.parse = parse
-# You need to do it for all the models you need
+def init_tweepy():
+	# Status() is the data model for a tweet
+	tweepy.models.Status.first_parse = tweepy.models.Status.parse
+	tweepy.models.Status.parse = parse
+	# User() is the data model for a user profil
+	tweepy.models.User.first_parse = tweepy.models.User.parse
+	tweepy.models.User.parse = parse
 
-auth = OAuthHandler(config['DEFAULT']['consumer_key'], config['DEFAULT']['consumer_secret'])
-auth.set_access_token(config['DEFAULT']['access_token'], config['DEFAULT']['access_secret'])
+def main():    
+	username = parse_arguments().username
+	config = parse_config('config.cfg')
+
+	auth = OAuthHandler(config['DEFAULT']['consumer_key'], config['DEFAULT']['consumer_secret'])
+	auth.set_access_token(config['DEFAULT']['access_token'], config['DEFAULT']['access_secret'])
  
-api = tweepy.API(auth)
+	api = tweepy.API(auth)
  
-tweets = []
+	tweets = []
 
-tweets = api.user_timeline(screen_name=username, count=200, include_rts=False, exclude_replies=True)
-last_id = tweets[-1].id
+	tweets = api.user_timeline(screen_name=username, count=200, include_rts=False, exclude_replies=True)
+	last_id = tweets[-1].id
 
-while (True):
-	more_tweets = api.user_timeline(screen_name=username, count=200, include_rts=False, exclude_replies=True, max_id=last_id-1)
-	if (len(more_tweets) == 0):
-		break
-	else:
-		last_id = more_tweets[-1].id-1
-		tweets = tweets + more_tweets
+	while (True):
+		more_tweets = api.user_timeline(screen_name=username, count=200, include_rts=False, exclude_replies=True, max_id=last_id-1)
+		if (len(more_tweets) == 0):
+			break
+		else:
+			last_id = more_tweets[-1].id-1
+			tweets = tweets + more_tweets
 
-media_files = []
-for status in tweets:
-	media = status.entities.get('media', []) 
-	if(len(media) > 0):
-		media_files.append(media[0]['media_url'])
+			media_files = []
+			for status in tweets:
+				media = status.entities.get('media', []) 
+				if(len(media) > 0):
+					media_files.append(media[0]['media_url'])
 
-for media_file in media_files:
-	wget.download(media_file)
+					for media_file in media_files:
+						wget.download(media_file)
+
+
+						if __name__=='__main__':
+							main()
