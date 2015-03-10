69dff4d042f9b7ba2f6e90e7332f498214372dde
Miguel Martinez-Alvarez <miguel.martinez@signal.uk.com>
2015-03-10 14:23:19 +0000

    Minor Refactor

--- src/run.py
+++ src/run.py
@@ -9,13 +9,10 @@ import configparser
 def parse_arguments():
 	parser = argparse.ArgumentParser(description='Download pictures from a Twitter feed.')
 	parser.add_argument('username', type=str, help='The twitter screen name from the account we want to retrieve all the pictures')
-
 	parser.add_argument('--num', type=int, help='Maximum number of tweets to be returned.')
 	parser.set_defaults(num=100)
-
 	parser.set_defaults(retweets=False)
 	parser.add_argument('--retweets', action='store_true', help='Include retweets')
-	
 	parser.set_defaults(replies=False)	
 	parser.add_argument('--replies', action='store_true', help='Include replies')
 	
@@ -24,8 +21,7 @@ def parse_arguments():
 
 def parse_config(config_file):
 	config = configparser.ConfigParser()
-	config.read(config_file)
-	# TODO: Return a dictionary with all the key-values
+	config.read(config_file)	
 	return config 
 	
 @classmethod
@@ -47,18 +43,13 @@ def authorise_twitter_api(config):
 	auth.set_access_token(config['DEFAULT']['access_token'], config['DEFAULT']['access_secret'])
 	return auth
 
-def download_images(api, username, retweets, replies, num_tweets):
- 	# Better efficiency, save every tweet as they come instead of reading them all?
+def download_images(api, username, retweets, replies, num_tweets): 
 	tweets = api.user_timeline(screen_name=username, count=200, include_rts=retweets, exclude_replies=replies)
 	last_id = tweets[-1].id
 
-	# TODO: Make more efficient 
 	downloaded = 0
-	while (True):		
-		if (len(tweets) == 0):
-			break
-		else:
-			last_id = tweets[-1].id-1
+	while (len(tweets) != 0):		
+		last_id = tweets[-1].id-1
 		
 		for status in tweets:
 			media = status.entities.get('media', []) 
@@ -69,7 +60,6 @@ def download_images(api, username, retweets, replies, num_tweets):
 		tweets = api.user_timeline(screen_name=username, count=200, include_rts=retweets, exclude_replies=replies, max_id=last_id-1)
 
 def main():    
-	#TODO: Restructure in a dictionary?
 	arguments = parse_arguments() 
 	username = arguments.username
 	retweets = arguments.retweets
