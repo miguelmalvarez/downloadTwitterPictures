bd369c22d2eb0b80ee2d72626ba032f0ad8649fd
Miguel Martinez <miguelmalvarez@gmail.com>
2018-08-26 11:39:41 +0100

    small test to run code with NASA and National Geographic pictures

--- bin/test.sh
+++ bin/test.sh
@@ -1,7 +1,7 @@
-python ../run.py --username NatGeoPhotos --num 10
+python ../run.py --username NatGeoPhotos --num 10 --config ../config.cfg
 mv pictures pictures-NatGeo
-python ../run.py --hashtag Photography --num 10
+python ../run.py --hashtag Photography --num 10 --config ../config.cfg
 mv pictures pictures-Photo
-python ../run.py --username NASA --num 10 --replies --retweets
+python ../run.py --username NASA --num 10 --replies --retweets --config ../config.cfg
 mv pictures pictures-NASA
-python ../run.py --username NASA --num 10 --replies --retweets --output NASA_Pictures
+python ../run.py --username NASA --num 10 --replies --retweets --output NASA_Pictures --config ../config.cfg
